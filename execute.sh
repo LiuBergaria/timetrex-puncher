d8cf1f04a0f01af2af993604182ffe937d61220e
Helil Barbosa Bergária <liubergaria@hotmail.com>
2022-03-11 12:11:45 -0300

    feat: Created execute.sh command

---
+++ execute.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Bater ponto
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 👊🏻
+
+export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+npm start
